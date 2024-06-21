539d5f91bdde34f3df34c9720272a131b6261bdc
Paulo Martins <pjfmartins83@gmail.com>
2024-06-21 10:14:31 -0300

    21/06/2024

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView
+from .views import TaskListView, TaskDetailView, TaskCreateView
 
 urlpatterns = [
+    path("task/new/", TaskCreateView.as_view(), name="task_new"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
     path("", TaskListView.as_view(), name="home"),
 ]
